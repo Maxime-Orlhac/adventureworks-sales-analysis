fcbf86ef0f00b337a53e8e804e4ec568f229b7b2
Maxime-Orlhac <maximeorlhac.pro@gmail.com>
2026-09-05 12:14:18 +0200

    Delete SQL_Sales_Quota.sql

--- SQL_Sales_Quota.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    FSQ.EmployeeKey,FSQ.Date,FSQ.SalesAmountQuota,
-    E.FirstName,E.LastName,E.HireDate,E.BirthDate,E.Gender,E.EmailAddress
-
-FROM FactSalesQuota AS FSQ
-
-JOIN DimEmployee AS E
-    ON FSQ.EmployeeKey = E.EmployeeKey;
